89a1b174aceb4b24f1d5967f829bdb2cb6e7b1c5
DanielOsorio01 <d.osorioc2@uniandes.edu.co>
2022-10-21 18:19:44 -0500

    solicitud borrar la fecha

--- solicitud/logic/logic_solicitud.py
+++ solicitud/logic/logic_solicitud.py
@@ -4,7 +4,6 @@ from ..models import Solicitud
 def create_solicitud(form):
     solicitud = Solicitud(
         cliente=form['cliente'],
-        fecha=form['fecha'],
         monto=form['monto'],
         cuotas=form['cuotas'],
         estado=form['estado']
