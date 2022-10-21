f5311664e03d64c3b7acbbd0584a34845de8801d
DanielOsorio01 <d.osorioc2@uniandes.edu.co>
2022-10-21 16:36:13 -0500

    urls.py

--- proyectoavanzo/urls.py
+++ proyectoavanzo/urls.py
@@ -21,6 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('', include('cliente.urls')),
     path('health-check/', views.healthCheck),
 
 ]
