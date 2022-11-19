c69e450886f818f9f8f64c3111265dc95cb758f3
DanielOsorio01 <d.osorioc2@uniandes.edu.co>
2022-11-19 00:12:45 -0500

    Update urls.py

--- solicitud/urls.py
+++ solicitud/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('solicitudes/', views.solicitud_list, name='solicitudList'),
-    path('solicitudes1/', views.solicitud_list, name='solicitudList'),
+    path('solicitudes1/', views.solicitud_list1, name='solicitudList'),
     path('solicitudview/', views.solicitudes_view, name='solicitudView'),
     path('solicitudcreate/', csrf_exempt(views.solicitud_create), name='solicitudcreate'),
     path('solicitudprocesar/', csrf_exempt(views.solicitud_procesar), name='solicitudprocesar'),
