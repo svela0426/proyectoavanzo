cd15a837a4393fbf9578f9d5eb5e032b86e843be
Nicolás Gómez <68575075+jngm8@users.noreply.github.com>
2022-12-09 02:06:59 -0500

    Update views.py

--- solicitud/views.py
+++ solicitud/views.py
@@ -10,7 +10,7 @@ from .logic.logic_solicitud import create_solicitud, process_solicitud, get_soli
 from django.contrib.auth.decorators import login_required
 from proyectoavanzo.auth0backend import getRole
 
-@login_required
+
 def solicitud_create(request):
     role = getRole(request)
     if role == "Cliente":
@@ -19,14 +19,14 @@ def solicitud_create(request):
             solicitud = serializers.serialize('json', [solicitud_dto])
             return HttpResponse(solicitud, content_type='application/json')
 
-@login_required
+
 def solicitud_procesar(request):
     if request.method == 'POST':
         respuesta = process_solicitud(json.loads(request.body))
         return HttpResponse(respuesta)
 
 
-@login_required
+
 def solicitud_list(request):
     role = getRole(request)
     if role == "Cliente":
